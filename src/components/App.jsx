261a64f8412340aa62a94aeb83d8d4271ab8e046
binodcoder <binodcoder@gmail.com>
2024-12-22 11:45:26 +0000

    making use of props

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,23 +1,27 @@
 import React from "react";
+import Contact from "./Contact";
 
 function App() {
   return (
     <div>
-      <h1 className="heading">My Contacts</h1>
-      <div className="card">
-        <div className="top">
-          <h2 className="name">Beyonce</h2>
-          <img
-            className="circle-img"
-            src="https://blackhistorywall.files.wordpress.com/2010/02/picture-device-independent-bitmap-119.jpg"
-            alt="avatar_img"
-          />
-        </div>
-        <div className="bottom">
-          <p>+123 456 789</p>
-          <p>b@beyonce.com</p>
-        </div>
-      </div>
+      <Contact
+        name="Beyonce"
+        img="https://blackhistorywall.files.wordpress.com/2010/02/picture-device-independent-bitmap-119.jpg"
+        tel="+123 456 789"
+        email="b@beyonce.com"
+      />
+      <Contact
+        name="Jack Bauer"
+        img="https://pbs.twimg.com/profile_images/625247595825246208/X3XLea04_400x400.jpg"
+        tel="+987 654 321"
+        email="jack@nowhere.com"
+      />
+      <Contact
+        name="Chuck Norris"
+        img="https://i.pinimg.com/originals/e3/94/47/e39447de921955826b1e498ccf9a39af.png"
+        tel="+918 372 574"
+        email="gmail@chucknorris.com"
+      />
     </div>
   );
 }
